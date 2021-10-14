f1e7da8b3edb8df267abb2b07d0755590d999451
Mattias Bonte <info@mattiasbonte.dev>
2021-10-14 14:56:52 +0200

    Add Tailwind and fix compat issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./scripts/*.js', 'index.html'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-gray': colors.blueGray,
+        'cool-gray': colors.coolGray,
+        gray: colors.gray,
+        'true-gray': colors.trueGray,
+        'warm-gray': colors.warmGray,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
+      },
+    },
   },
   variants: {
     extend: {},
